a2348d888e0103da34e6ec8e3c506841b536a94d
Rana <85198305+RanaAbdulkareem@users.noreply.github.com>
2023-06-15 01:40:12 +0300

    Update Case Study 2 - Solution A.sql

--- Week 2 - Case Study #2 Pizza Runner/Case Study 2 - Solution A.sql
+++ Week 2 - Case Study #2 Pizza Runner/Case Study 2 - Solution A.sql
@@ -5,18 +5,19 @@
 
 UPDATE runners
 SET registration_date = date_sub(registration_date, INTERVAL 1 YEAR);
-
 ------------------------------------------------------------
 
 -- Second: Cleaning customer_orders table 
 
 UPDATE customer_orders
 SET exclusions = CASE 
-		 WHEN (exclusions = '' OR exclusions = 'null') THEN null
+		 WHEN (exclusions = '' OR exclusions = 'null') 
+	         THEN null
 		 ELSE exclusions 
 		 END,
 	extras = CASE 
-                 WHEN (extras = '' OR extras = 'null') THEN null
+                 WHEN (extras = '' OR extras = 'null') 
+	         THEN null
                  ELSE extras 
                  END;
 
@@ -26,11 +27,13 @@ SET exclusions = CASE
 
 UPDATE runner_orders
 SET pickup_time = CASE 
-                  WHEN (pickup_time = '' OR pickup_time = 'null') THEN NULL
+                  WHEN (pickup_time = '' OR pickup_time = 'null') 
+	          THEN NULL
                   ELSE pickup_time 
                   END,
        distance = CASE 
-                  WHEN (distance = '' OR distance = 'null') THEN NULL
+                  WHEN (distance = '' OR distance = 'null') 
+	          THEN NULL
                   ELSE distance 
                   END,
        duration = CASE 
@@ -38,14 +41,11 @@ SET pickup_time = CASE
                   ELSE duration 
                   END,
    cancellation = CASE 
-                  WHEN (cancellation = '' OR cancellation = 'null') THEN NULL
+                  WHEN (cancellation = '' OR cancellation = 'null') 
+	          THEN NULL
                   ELSE cancellation 
                   END;
-
---------------------------------
-UPDATE runner_orders
-SET pickup_time = '2020-01-02 13:12:37'
-WHERE order_id = 3;
+ 
 ----------------------------------
 
 UPDATE runner_orders
@@ -55,8 +55,8 @@ SET distance = regexp_replace(distance, "[a-z]", ''),
 
 ALTER TABLE runner_orders
 MODIFY pickup_time timestamp,
-MODIFY distance decimal(5,2),
-MODIFY duration decimal(5,2);
+MODIFY distance float,
+MODIFY duration int;
 
 ------------------------------------------------------------
 
@@ -65,7 +65,7 @@ MODIFY duration decimal(5,2);
 
 -- 1.How many pizzas were ordered?
 
-SELECT COUNT(pizza_id) AS num_of_pizza_ordered
+SELECT COUNT(pizza_id) AS orders
 FROM customer_orders;
 
 ------------------------------------------------------------
@@ -79,7 +79,8 @@ FROM customer_orders;
 
 -- 3.How many successful orders were delivered by each runner? 
 
-SELECT runner_id, count(*) AS num_orders
+SELECT runner_id, 
+       count(*) AS successful_orders
 FROM runner_orders
 WHERE cancellation IS NULL
 GROUP BY runner_id;
@@ -88,7 +89,8 @@ GROUP BY runner_id;
 
 -- 4. How many of each type of pizza was delivered? 
 
-SELECT piz.pizza_name, COUNT(cus_ord.pizza_id) num
+SELECT piz.pizza_name, 
+       count(cus_ord.pizza_id) delivered
 FROM pizza_names piz
 JOIN customer_orders cus_ord
 ON cus_ord.pizza_id = piz.pizza_id
@@ -100,11 +102,13 @@ GROUP BY piz.pizza_name;
 
 -- ANOTHER SOLUTION
 SELECT SUM(CASE
-           WHEN piz_nam.pizza_name = 'Meatlovers' THEN 1 
+           WHEN piz_nam.pizza_name = 'Meatlovers' 
+	   THEN 1 
 	   ELSE 0 
            END) AS Meat_Lovers,
 	   SUM(CASE 
-           WHEN piz_nam.pizza_name = 'Vegetarian' THEN 1
+           WHEN piz_nam.pizza_name = 'Vegetarian' 
+           THEN 1
            ELSE 0 
            END) AS Vegetarian
 		
@@ -118,20 +122,22 @@ ON run_ord.order_id = cus_ord.order_id AND run_ord.cancellation IS NULL;
 
 -- 5.How many Vegetarian and Meatlovers were ordered by each customer? 
 
-SELECT cus_ord.customer_id,
-                  SUM(CASE 
-		      WHEN piz.pizza_name = 'Meatlovers' THEN 1 
-                      ELSE 0 
-                      END) AS meatlovers_ordered,
-	              SUM(CASE 
-                      WHEN piz.pizza_name = 'Vegetarian' THEN 1
-                      ELSE 0 
-                      END) AS vegetarian_ordered
+SELECT customer_id,
+       SUM(CASE 
+           WHEN piz.pizza_name = 'Meatlovers' 
+	   THEN 1 
+           ELSE 0 
+	   END) AS meatlovers_ordered,
+       SUM(CASE 
+           WHEN piz.pizza_name = 'Vegetarian' 
+	   THEN 1
+           ELSE 0 
+	   END) AS vegetarian_ordered
            
 FROM pizza_names piz
 JOIN customer_orders cus_ord
 ON cus_ord.pizza_id = piz.pizza_id
-GROUP BY cus_ord.customer_id;
+GROUP BY customer_id;
 
 ------------------------------------------------------------
 
@@ -139,7 +145,8 @@ GROUP BY cus_ord.customer_id;
 
 WITH orders_count AS (
 
-SELECT cus_ord.order_id AS order_id, count(cus_ord.pizza_id) AS num_of_pizza
+SELECT cus_ord.order_id AS order_id, 
+       count(cus_ord.pizza_id) AS quantity
 FROM customer_orders cus_ord
 JOIN runner_orders run_ord
 ON run_ord.order_id = cus_ord.order_id
@@ -147,28 +154,30 @@ WHERE run_ord.cancellation IS NULL
 GROUP BY cus_ord.order_id
 )
 
-SELECT max(num_of_pizza) AS max_pizza_ordered
+SELECT max(quantity) AS maximum_order
 FROM orders_count;
 
 ------------------------------------------------------------
 
 -- 7.For each customer, how many delivered pizzas had at least 1 change and how many had no changes? 
 
-SELECT cus_ord.customer_id,
-	           SUM(CASE 
-		       WHEN exclusions IS NOT NULL OR extras IS NOT NULL THEN 1
-                       ELSE 0 
-                       END) AS changed_,
-                   SUM(CASE 
-                       WHEN exclusions IS NULL AND extras IS NULL THEN 1
-                       ELSE 0 
-                       END) AS not_changed
+SELECT customer_id,
+       SUM(CASE  
+	   WHEN exclusions IS NOT NULL OR extras IS NOT NULL 
+	   THEN 1 
+	   ELSE 0 
+	   END) AS changed_,
+        SUM(CASE 
+	    WHEN exclusions IS NULL AND extras IS NULL 
+	    THEN 1
+            ELSE 0  
+	    END) AS not_changed
                   
 FROM customer_orders cus_ord
 JOIN runner_orders run_ord
 ON run_ord.order_id = cus_ord.order_id
 WHERE run_ord.cancellation IS NULL
-GROUP BY cus_ord.customer_id;
+GROUP BY customer_id;
 
 ------------------------------------------------------------
 
@@ -178,22 +187,25 @@ SELECT COUNT(*) AS num_pizza
 FROM customer_orders cus_ord
 JOIN runner_orders run_ord
 ON run_ord.order_id = cus_ord.order_id
-WHERE run_ord.cancellation IS NULL AND (cus_ord.exclusions IS NOT NULL AND extras IS NOT NULL);
+WHERE run_ord.cancellation IS NULL 
+      AND (cus_ord.exclusions IS NOT NULL AND extras IS NOT NULL);
 
 ------------------------------------------------------------
 
 -- 9. What was the total volume of pizzas ordered for each hour of the day?
 
-SELECT EXTRACT(hour FROM order_time) AS hour_of_the_day, COUNT(*) AS num_pizza
+SELECT EXTRACT(hour FROM order_time) AS hour_, 
+       COUNT(*) AS quantity
 FROM customer_orders
-GROUP BY hour_of_the_day
-ORDER BY hour_of_the_day;
+GROUP BY hour_
+ORDER BY hour_;
 
 ------------------------------------------------------------
 
 -- 10. What was the volume of orders for each day of the week? 
 
-SELECT DAYNAME(order_time) AS week_day, COUNT(distinct order_id) AS num_pizza
+SELECT DAYNAME(order_time) AS week_day, 
+       COUNT(distinct order_id) AS num_pizza
 FROM customer_orders
 GROUP BY week_day
 ORDER BY week_day;
